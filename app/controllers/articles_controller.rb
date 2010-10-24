e038aa7dbae351bbf0a4acc3916ca92c7a02ccce
Ben Griffiths <bengoldcross@googlemail.com>
2010-10-24 02:29:39 +0100

    A snazzy title and find by month links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ before_filter :authenticate_user!, :except => [ :index, :show ]
   # GET /articles
   # GET /articles.xml
   def index
-    @articles = Article.all.reverse
+    @articles = find_by_month(params[:month],params[:year]) || Article.all.reverse
 
     respond_to do |format|
       format.html # index.html.erb
@@ -83,6 +83,52 @@ before_filter :authenticate_user!, :except => [ :index, :show ]
     respond_to do |format|
       format.html { redirect_to(articles_url) }
       format.xml  { head :ok }
+    end
+  end
+  
+  
+  private
+  def find_by_month(month, year)
+    if month.nil?
+      # no month means no params - default to all articles by returning nil
+      nil
+    else
+      # find a range of articles from start_date to end_date
+      start_date = Time.gm(year, month, 1, 00,00,00)
+      end_date = Time.gm(year, month, last_day(month, year), 00,00,00)
+      Article.find(:all, :conditions => {:created_at => start_date..end_date}, :order => "created_at DESC")
+    end
+  end
+    
+  # Determines the date of the last day on the month i.e. 28, 29, 30 or 31
+  def last_day(month, year)
+    case month
+    when 1 then 31
+    when 2 then leap(year)
+    when 3 then 31
+    when 4 then 30
+    when 5 then 31
+    when 6 then 30
+    when 7 then 31
+    when 8 then 31
+    when 9 then 30
+    when 10 then 31
+    when 11 then 30
+    when 12 then 31
+    else 31
+    end
+  end
+  
+  # Calculate the leap year
+  def leap(year)
+    if year%400 == 0
+      29
+    elsif year%100 == 0
+      28
+    elsif year%4 == 0
+      29
+    else
+      28
     end
   end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -7,6 +7,11 @@ module ArticlesHelper
     end
   end
   
+  def title_pic
+    lang = ['ruby', 'java', 'python', 'cplusplus']
+    image_tag("/images/title-#{lang[rand(4)]}.png", :id => 'title')
+  end
+  
   private
   def current_page?(name, path)
     if name == 'Ben' && path == '/ben'
@@ -19,4 +24,83 @@ module ArticlesHelper
       false
     end
   end
+  
+  def select_by_month
+    articles = Article.find(:all)
+    date_list = DateList.new
+    
+    articles.each do |a|
+      if date_list.new_month?(a.created_at.month, a.created_at.year)
+        date_list.add(a.created_at.month, a.created_at.year)
+      else
+        date_list.increment(a.created_at.month, a.created_at.year)
+      end
+    end
+    
+    html ="" 
+    date_list.dates.each do |list|
+      html+=link_to("#{to_month(list[:month])} #{list[:year].to_s[2..3]} (#{list[:count]})", 
+                                        articles_path(:month => list[:month], :year => list[:year]))+"<br />"
+    end
+    
+    return html
+  end
+  
+  private
+  
+  def to_month(num)
+      case num
+      when 1 then "January"
+      when 2 then "February"
+      when 3 then "March"
+      when 4 then "April"
+      when 5 then "May"
+      when 6 then "June"
+      when 7 then "July"
+      when 8 then "August"
+      when 9 then "September"
+      when 10 then "October"
+      when 11 then "November"
+      when 12 then "December"
+      else "Unknown Month...?"
+      end
+  end
+  
+  class DateList
+    
+    attr_reader :dates
+    
+    def initialize
+      @dates = Array.new
+    end
+    
+    def new_month?(month, year)
+      @dates.each do |d|
+        if d[:month] == month && d[:year] == year
+          return false
+        end
+      end
+      true
+    end
+    
+    def increment(month, year)
+      @dates.each do |d|
+        if d[:month] == month && d[:year] == year
+          d[:count]+=1
+          return true
+        end
+      end
+      return false
+    end
+    
+    def add(month, year)
+      new_month = {
+        :month => month,
+        :year => year,
+        :count => 1
+      }
+      @dates.push(new_month)
+    end
+  end
+  
 end
